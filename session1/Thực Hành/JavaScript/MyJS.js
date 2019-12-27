684e0ad62096f77d1e1d01363d9df906d7e47f8c
khanh Nguyen <khanhkim3397@gmail.com>
2019-12-27 10:02:57 +0700

    ubpdate Bai Tap

---
+++ session1/Thực Hành/JavaScript/MyJS.js
@@ -0,0 +1,21 @@
+// let f = 20.5
+// let b = true
+// let s = 'Hà Nội'
+//
+// document.write('i = ' + i)
+// document.write('<br/>')
+// document.write('f = ' + f)
+// document.write('<br/>')
+// document.write('b = ' + b)
+// document.write('<br/>')
+// document.write('s = ' + s)
+// let width = 20
+// let height = 20
+// let area = width * height
+// document.write('Area = ' + area)
+let inputNumberA
+let inputNumerB
+inputNumberA = prompt("Enter the Number")
+inputNumerB = prompt("Enter the Number")
+let area = inputNumberA / inputNumerB
+document.write('Area = ' +area)
--- session2/BTChuyenDoiTIenTe/MyJS.js
+++ session2/BTChuyenDoiTIenTe/MyJS.js
@@ -1,1 +1,15 @@
-let Amount;
+function  myFunction() {
+    let amount = document.getElementById('amout').value;
+    let frCurrency = document.getElementById('frCurrency').value;
+    let toCurrency = document.getElementById('toCurrency').value;
+    let result = document.getElementById('result');
+    if (frCurrency =='VND' && toCurrency == 'USD') {
+        result.innerText = frCurrency / 23000;
+
+    }else (frCurrency =='USD' && toCurrency == 'VND')
+    {
+        result.innerText = frCurrency * 23000;
+    }
+
+
+}
