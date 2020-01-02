fc87bc5fa3c2d93691bfcfafe6c16648021a7860
khanh Nguyen <khanhkim3397@gmail.com>
2020-01-02 11:15:19 +0700

    update bai tap

--- session2/BienKieuDuLieuToanTu/Bt2.js
+++ session2/BienKieuDuLieuToanTu/Bt2.js
@@ -1,5 +1,5 @@
 let Celsius = parseInt(prompt("Nhap nhiet do can doi: "));
-document.write("Do C:" +Celsius);
+document.write("Do C:" +Celsius );
 document.write("<br>");
 let F = (Celsius * 1.8)+32;
 document.write("Do F:" +F);
